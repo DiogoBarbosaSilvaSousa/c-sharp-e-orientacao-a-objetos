3d070a8109ad121873640433550c74458d8fe6e0
Diogo Barbosa Silva Sousa <diogobarbosasilva@gmail.com>
2021-08-17 20:07:31 -0300

    Projeto ByteBank - 03

---
+++ ByteBank/03-ByteBank/Program.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _03_ByteBank
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            ContaCorrente contaDaGabriela = new ContaCorrente();
+            contaDaGabriela.titular = "Gabriela";
+            contaDaGabriela.agencia = 863;
+            contaDaGabriela.numero = 863452;
+
+            ContaCorrente contaDaGabrielaDaCosta = new ContaCorrente();
+            contaDaGabrielaDaCosta.titular = "Gabriela";
+            contaDaGabrielaDaCosta.agencia = 863;
+            contaDaGabrielaDaCosta.numero = 863452;
+
+            Console.WriteLine("Igualdade de tipo de referência: " + (contaDaGabriela == contaDaGabrielaDaCosta));
+
+            int idade = 27;
+            int idadeMaisUmaVez = 27;
+
+            Console.WriteLine("Igualdade de tipo de valor: " + (idade == idadeMaisUmaVez));
+
+            contaDaGabriela = contaDaGabrielaDaCosta;
+            Console.WriteLine(contaDaGabriela == contaDaGabrielaDaCosta);
+
+            contaDaGabriela.saldo = 300;
+            Console.WriteLine(contaDaGabriela.saldo);
+            Console.WriteLine(contaDaGabrielaDaCosta.saldo);
+
+            if (contaDaGabriela.saldo >= 100)
+            {
+                contaDaGabriela.saldo -= 100;
+            }
+
+            Console.ReadLine();
+        }
+    }
+}
